2eda55d6bb9978c45344220d52f17c72fc50ba8c
snaggytoothy <snaggytoothy@gmail.com>
2025-01-27 12:47:13 +0900

    Added lecutres 1 to 4
    강의 1-4까지 추가

---
+++ Grammar_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class program
+    {
+        static void Main(string[] args) //가장 처음 실행 됨
+        {
+            Console.WriteLine("Hello World!"); //새 줄 시작
+            Console.WriteLine("My Name\n is \tKero");
+            Console.WriteLine("My Name\r is \bKero");
+
+            Console.WriteLine(10);
+            Console.WriteLine(3.141592);
+            Console.WriteLine(3 + 3);
+        }
+    }
+}
---
+++ Grammar_2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _3_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+
+            Console.Write("Enter two numbers: ");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            int num1 = int.Parse(numbers[0]);
+            int num2 = int.Parse(numbers[1]);
+
+            int sum = num1 + num2;
+
+            Console.Write("The sum of {0} and {1} is {2}.", num1, num2, sum);
+        }
+    }
+}
+
