654856848d32b518d38525ce6682aef2a592f437
snaggytoothy <snaggytoothy@gmail.com>
2025-01-27 15:41:44 +0900

    240127_1541
    1-5까지 작성

--- Grammar_3/Program.cs
+++ Grammar_3/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace _1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10, num2 = 20;
+
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+            Console.WriteLine(num1 % num2);
+
+            Console.WriteLine();
+
+            Console.WriteLine("관계연산자");
+            Console.WriteLine(num1 == num2);
+            Console.WriteLine(num1 != num2);
+            Console.WriteLine(num1 > num2);
+            Console.WriteLine(num1 < num2);
+            Console.WriteLine(num1 >= num2);
+            Console.WriteLine(num1 <= num2);
+            Console.WriteLine();
+
+            Console.WriteLine("논리연산자");
+
+            int num3 = 15;
+
+            Console.WriteLine(0 <= num3 && num3 <= 20);
+            Console.WriteLine(0 > num3 || num3 > 20);
+
+            Console.WriteLine();
+
+            string str1 = "Apple";
+            string str2 = "apple";
+            int compare = string.Compare(str1, str2);
+            Console.WriteLine(compare);
+        }
+    }
+}
